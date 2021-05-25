ea44e0cbcef2792434f063a948f7c627c406448f
nitinchouhan1010 <nitinchouhan4444@gmail.com>
2021-05-25 17:49:13 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 import paginationFactory from 'react-bootstrap-table2-paginator';
+import { Table } from 'reactstrap';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import cellEditFactory from 'react-bootstrap-table2-editor';
 
 const { SearchBar } = Search;
 
@@ -80,12 +85,37 @@ export default function App() {
             <SearchBar {...props.searchProps} />
             <hr />
             <BootstrapTable
+              cellEdit={cellEditFactory({ mode: 'click' })}
               {...props.baseProps}
               pagination={paginationFactory()}
             />
           </div>
         )}
       </ToolkitProvider>
+      <Table className="broker-table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td scope="row">2</td>
+            <td>Jacob</td>
+            <td>tdornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr className="light-red">
+            <td scope="row">1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
