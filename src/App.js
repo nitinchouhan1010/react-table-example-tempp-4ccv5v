36e0bf484c2397e3ede3a26460cd3b521ab7524a
nitinchouhan1010 <nitinchouhan4444@gmail.com>
2021-05-25 18:29:32 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 import paginationFactory from 'react-bootstrap-table2-paginator';
-import { Table } from 'reactstrap';
+import { Table, Input } from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
@@ -17,23 +17,35 @@ export default function App() {
   const columns = [
     {
       dataField: 'id',
-      text: 'Product ID'
+      text: 'Product ID',
+      headerStyle: {
+        backgroundColor: '#565082cc',
+        color: '#fff'
+      }
     },
     {
       dataField: 'name',
-      text: 'Product Name'
+      text: 'Product Name',
+      headerStyle: {
+        backgroundColor: '#565082cc',
+        color: '#fff'
+      }
     },
     {
       dataField: 'price',
-      text: 'Product Price'
+      text: 'Product Price',
+      headerStyle: {
+        backgroundColor: '#565082cc',
+        color: '#fff'
+      }
     }
   ];
 
   var products = [
     {
       id: 1,
-      name: 'type1',
-      price: 123
+      name: <input type="text" id="fname" name="fname" />,
+      price: <input type="text" id="lname" name="lname" />
     },
     {
       id: 9,
@@ -85,7 +97,6 @@ export default function App() {
             <SearchBar {...props.searchProps} />
             <hr />
             <BootstrapTable
-              cellEdit={cellEditFactory({ mode: 'click' })}
               {...props.baseProps}
               pagination={paginationFactory()}
             />
@@ -110,9 +121,13 @@ export default function App() {
           </tr>
           <tr className="light-red">
             <td scope="row">1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>Jacob</td>
+            <td>
+              <Input type="text" id="fname" name="fname" size="sm" />
+            </td>
+            <td>
+              <Input type="text" id="fname" name="fname" size="sm" />
+            </td>
           </tr>
         </tbody>
       </Table>
